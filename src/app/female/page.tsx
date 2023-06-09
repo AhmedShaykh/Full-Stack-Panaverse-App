3ea78d3f14446dce90fc2324309992a4165dc2ed
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2023-06-09 23:32:18 +0500

    Add Female Category

--- src/app/female/page.tsx
+++ src/app/female/page.tsx
@@ -1,11 +1,61 @@
 import React from 'react';
 import Navbar from '@/Components/Navbar';
+import Womens from '@/Components/Womens';
 import Footer from '@/Components/Footer';
+import { client } from '../../../sanity/lib/client';
+import { Image } from 'sanity';
 
-const Female = () => {
+export const getProductData = async () => {
+
+    const res = await client.fetch(`
+    *[_type=="product" && category -> name == "Female"] {
+        title,
+        image,
+        price,
+        category -> {
+            name
+        },
+        dresstype -> {
+            name
+        }
+      }
+    `);
+
+    return res;
+};
+
+interface IProduct {
+    title: string;
+    image: Image;
+    price: number;
+    category: {
+        name: string;
+    }
+    dresstype: {
+        name: string;
+    }
+};
+
+const Female = async () => {
+
+    const data: IProduct[] = await getProductData();
+
     return (
         <>
             <Navbar />
+            <div className="my-8 grid grid-cols-[repeat(3,auto)] justify-center gap-x-10 gap-y-12">
+                {data.map((item, i: number) => (
+                    <div key={i}>
+                        <Womens
+                            title={item.title}
+                            image={item.image}
+                            price={item.price}
+                            category={item.category.name}
+                            dresstype={item.dresstype.name}
+                        />
+                    </div>
+                ))}
+            </div>
             <Footer />
         </>
     )
