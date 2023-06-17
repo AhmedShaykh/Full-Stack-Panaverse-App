13d98f98cee20d63257c849bd2ed23e89680f56b
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2023-06-17 20:41:34 +0500

    Add Dynamic

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -1,10 +1,28 @@
 import React from "react";
+import { client } from "@/lib/sanityClient";
 
-const getProductsId = () => { };
+const getProductById = async (id: string) => {
+  const res = await client.fetch(`
+    *[_type == "product" && _id == ${id}] {
+      title,
+      image,
+      price,
+      category {
+        name
+      },
+      dresstype {
+        name
+      }
+    }
+  `);
 
-const Product = async () => {
+  return res[0];
+};
+
+const Product = () => {
   return (
     <div className="my-16 mx-24">
+      <h1>Dynamic</h1>
     </div>
   );
 };
