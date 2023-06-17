ade9476e1ff31a5859a98172277cbc6fa323599d
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2023-06-17 23:15:28 +0500

    Update page.tsx

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -1,14 +1,15 @@
-import React from "react";
+"use client";
+import React, { useState, useEffect } from "react";
 import { client } from "@/lib/sanityClient";
 import { Image } from "sanity";
+
 interface Props {
   params: {
     id: string;
-  }
-};
+  };
+}
 
 const getProducts = async (id: string) => {
-
   const res = await client.fetch(`
     *[_type == "product" && _id == ${id}] {
       title,
@@ -32,23 +33,41 @@ interface IProduct {
   price: number;
   category: {
     name: string;
-  }
-  dresstype: {
-    name: string;
-  }
-};
+  };
+  dresstype?: { // Make this optional since it's not always present in the response
+    name?: string;
+  };
+}
 
-const Product = async ({ params: { id } }: Props) => {
+const Product = ({ params }: Props) => {
 
-  const product: IProduct[] = await getProducts(id);
+  const [product, setProduct] = useState<IProduct | null>(null);
+
+  useEffect(() => {
+
+    const fetchData = async () => {
+
+      try {
+
+        const productData = await getProducts(params.id);
+        setProduct(productData);
+
+      } catch (error) {
+
+        console.error(error);
+      }
+
+    };
+
+    fetchData();
+
+  }, [params.id]);
+
+  if (!product) return <div className="my-16 mx-24">Loading...</div>;
 
   return (
     <div className="my-16 mx-24">
-      {product?.map((item, i: number) => (
-        <div key={i}>
-          <h1>{item.title}</h1>
-        </div>
-      ))}
+      <h1>{product.title}</h1>
     </div>
   );
 };
