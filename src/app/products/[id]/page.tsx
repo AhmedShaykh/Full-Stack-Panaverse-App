a972c6fbcb6d96d8117efa75d533fd01521abaea
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2023-06-17 22:59:25 +0500

    Update page.tsx

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 import { client } from "@/lib/sanityClient";
+import { Image } from "sanity";
+interface Props {
+  params: {
+    id: string;
+  }
+};
 
-const getProductById = async (id: string) => {
+const getProducts = async (id: string) => {
+
   const res = await client.fetch(`
     *[_type == "product" && _id == ${id}] {
       title,
@@ -19,10 +26,29 @@ const getProductById = async (id: string) => {
   return res[0];
 };
 
-const Product = () => {
+interface IProduct {
+  title: string;
+  image: Image;
+  price: number;
+  category: {
+    name: string;
+  }
+  dresstype: {
+    name: string;
+  }
+};
+
+const Product = async ({ params: { id } }: Props) => {
+
+  const product: IProduct[] = await getProducts(id);
+
   return (
     <div className="my-16 mx-24">
-      <h1>Dynamic</h1>
+      {product?.map((item, i: number) => (
+        <div key={i}>
+          <h1>{item.title}</h1>
+        </div>
+      ))}
     </div>
   );
 };
