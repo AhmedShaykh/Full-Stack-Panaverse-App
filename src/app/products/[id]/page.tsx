847e9fe52440d9d9b5e9dc4b270656a4ea1db182
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2023-06-20 14:47:08 +0500

    Add Redux

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -1,13 +1,11 @@
+// "use client";
 import React from "react";
 import { client } from "@/lib/sanityClient";
+// import { cartActions } from "@/Store/slice/CartSlice";
+// import { useDispatch } from "react-redux";
 import { urlForImage } from "../../../../sanity/lib/image";
 import Image from "next/image";
-
-type Props = {
-  params: {
-    id: string;
-  }
-};
+// import toast from "react-hot-toast";
 
 const getProductById = async (id: any) => {
 
@@ -34,14 +32,25 @@ const Product = async ({ params }: any) => {
 
   const product: any = await getProductById(id);
 
+  // const dispatch = useDispatch();
+
+  // const addItems = () => {
+  //   dispatch(cartActions.addToCart({
+  //     quantity: 1
+  //   }));
+  //   toast.success("Product Added")
+  // };
+
   return (
     <div className="my-16 mx-24">
       <h1>
         {product.title}
       </h1>
+
       <h2>
         {product.price}
       </h2>
+
       <Image
         src={urlForImage(product.image).url()}
         alt="product"
@@ -49,6 +58,13 @@ const Product = async ({ params }: any) => {
         height={300}
         className="my-4 object-cover max-h-[300px]"
       />
+
+      <button
+        className="my-2 py-2 px-6 rounded bg-blue-700 text-white font-semibold"
+      // onClick={addItems}
+      >
+        Add To Cart
+      </button>
     </div>
   );
 };
