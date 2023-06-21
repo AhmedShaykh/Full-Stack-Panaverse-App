877e25e5e3c0599b09e019dc302907fdfbeb191c
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2023-06-21 23:57:52 +0500

    Minor Change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,13 @@ import Hero from "@/Components/Hero";
 import Event from "@/Components/Event";
 import Products from "@/Components/Products";
 
-const Home = async () => {
+const Home = () => {
     return (
         <>
             <Hero />
             <Event />
             {/* @ts-ignore */}
-            <Products />
+            {/* <Products /> */}
         </>
     )
 };
