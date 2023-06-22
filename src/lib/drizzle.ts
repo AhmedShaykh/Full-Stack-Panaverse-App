b04a0faf187e3558f2df98053c512a0e6e1019a9
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2023-06-22 21:58:22 +0500

    Create Drizzle Schema

---
+++ src/lib/drizzle.ts
@@ -0,0 +1,26 @@
+import {
+    pgTable,
+    serial,
+    varchar,
+    integer
+} from "drizzle-orm/pg-core";
+import { drizzle } from "drizzle-orm/vercel-postgres";
+import { InferModel } from "drizzle-orm";
+import { sql } from "@vercel/postgres";
+
+export const cartTable = pgTable("cart", {
+    id: serial("id").primaryKey(),
+    user_id: varchar("user_id", {
+        length: 255
+    }).notNull(),
+    product_id: varchar("product_id", {
+        length: 255
+    }).notNull(),
+    quantity: integer("quantity").notNull()
+});
+
+export type Cart = InferModel<typeof cartTable>;
+
+export type AddCart = InferModel<typeof cartTable, "insert">;
+
+export const db = drizzle(sql);
