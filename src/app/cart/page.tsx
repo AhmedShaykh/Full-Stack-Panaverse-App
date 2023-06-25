cf07c066ad89b98471f5475b182e5032fa1cbc08
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2023-06-25 22:19:34 +0500

    API Call In Cart Page

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,8 +1,58 @@
 import React from "react";
 
-const Cart = () => {
+const getData = async () => {
+
+    try {
+
+        const res = await fetch("http://127.0.0.1:3000/api/cart", {
+            method: "GET",
+            cache: "no-store",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        });
+
+        if (!res.ok) {
+            throw new Error("Failed to Fetch the Data")
+        };
+
+        const result = await res.json();
+
+        return result;
+    }
+    catch (error) {
+
+        console.log(error);
+
+    }
+
+};
+
+const Cart = async () => {
+
+    const data: any = await getData();
+
     return (
         <>
+            {data?.res.length > 0 ? (
+                <div>
+                    {data.res.map((item: any, i: number) => (
+                        <div key={i}
+                            className="my-16 mx-12 sm:mx-24"
+                        >
+                            <h1>
+                                {item.user_id}
+                            </h1>
+                        </div>
+                    ))}
+                </div>
+            ) : (
+                <div className="my-16 mx-12 sm:mx-24 flex justify-center items-center">
+                    <h1 className="text-4xl md:text-5xl font-extrabold leading-[3rem]">
+                        Empty Cart
+                    </h1>
+                </div>
+            )}
         </>
     )
 };
