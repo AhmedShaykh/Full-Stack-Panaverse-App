345203bcc7407163f59515054e3ebba8fdfd48c9
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2023-06-28 15:37:28 +0500

    Update page.tsx

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -50,36 +50,39 @@ const Cart = async () => {
     const data = await getData();
 
     const products = data?.res.map((item: any) => {
-        return item
+        return item.product_id
     });
 
-    // const product = await getProductById(products.res[0].product_id);
 
-    console.log(products);
+    products.forEach(async (res: any) => {
 
-    return (
-        <>
-            {products?.length > 0 ? (
-                <div>
-                    {products.map((item: any, i: number) => (
-                        <div key={i}
-                            className="my-16 mx-12 sm:mx-24"
-                        >
-                            <h1>
-                                {item.product_id}
-                            </h1>
-                        </div>
-                    ))}
-                </div>
-            ) : (
-                <div className="my-16 mx-12 sm:mx-24 flex justify-center items-center">
-                    <h1 className="text-4xl md:text-5xl font-extrabold leading-[3rem]">
-                        Empty Cart
-                    </h1>
-                </div>
-            )}
-        </>
-    )
+        const product = await getProductById(res);
+
+        return (
+            <>
+                <h1>{product?.title}</h1>
+                {/* {product?.length > 0 ? (
+                    <div className="my-16 mx-12 sm:mx-24">
+                        {product?.map((item: any, i: number) => {
+                            console.log('Item:', item); // Check the item structure
+                            return (
+                                <div key={i}>
+                                    <h1>{item.title}</h1>
+                                </div>
+                            );
+                        })}
+
+                    </div>
+                ) : (
+                    <div className="my-16 mx-12 sm:mx-24 flex justify-center items-center">
+                        <h1 className="text-4xl md:text-5xl font-extrabold leading-[3rem]">
+                            Empty Cart
+                        </h1>
+                    </div>
+                )} */}
+            </>
+        )
+    });
 };
 
 export default Cart;
