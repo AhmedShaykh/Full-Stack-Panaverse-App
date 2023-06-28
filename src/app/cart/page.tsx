81d19265c402de514a81aa6ff4415835c53c8a4e
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2023-06-28 15:48:35 +0500

    Update page.tsx

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -57,31 +57,30 @@ const Cart = async () => {
     products.forEach(async (res: any) => {
 
         const product = await getProductById(res);
+        console.log(product);
 
-        return (
+        // return (
             <>
-                <h1>{product?.title}</h1>
                 {/* {product?.length > 0 ? (
                     <div className="my-16 mx-12 sm:mx-24">
                         {product?.map((item: any, i: number) => {
-                            console.log('Item:', item); // Check the item structure
+                            console.log('Item:', item);
                             return (
                                 <div key={i}>
                                     <h1>{item.title}</h1>
                                 </div>
                             );
                         })}
-
                     </div>
-                ) : (
+                ) : ( */}
                     <div className="my-16 mx-12 sm:mx-24 flex justify-center items-center">
                         <h1 className="text-4xl md:text-5xl font-extrabold leading-[3rem]">
                             Empty Cart
                         </h1>
                     </div>
-                )} */}
+                {/* )} */}
             </>
-        )
+        // )
     });
 };
 
