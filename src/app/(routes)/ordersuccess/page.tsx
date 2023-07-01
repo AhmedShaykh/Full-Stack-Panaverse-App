462e6d670ec695e46366786b1b58fdfad1bd314e
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2023-07-01 14:59:24 +0500

    Create Order Success Page

--- src/app/(routes)/ordersuccess/page.tsx
+++ src/app/(routes)/ordersuccess/page.tsx
@@ -1,7 +1,12 @@
+"use client";
 import React from "react";
+import { useRouter } from "next/navigation";
 import { BsBagCheckFill } from 'react-icons/bs';
 
 const OrderSuccess = () => {
+
+    const router = useRouter();
+
     return (
         <div className="my-16 mx-12 sm:mx-24 flex flex-col justify-center items-center">
 
@@ -15,6 +20,13 @@ const OrderSuccess = () => {
             <h1 className="text-4xl md:text-5xl font-extrabold leading-[3rem]">
                 Thank you for your order!
             </h1>
+
+            <button
+                className="mt-8 mb-5 py-3 px-6 sm:py-4 sm:px-8 rounded bg-black text-xl sm:text-2xl text-white font-bold"
+                onClick={() => router.push("/")}
+            >
+                Continue Shopping
+            </button>
         </div>
     )
 };
