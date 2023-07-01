5c5e8e65d80d1ec95d75105594286fb2e973f185
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2023-07-01 22:37:51 +0500

    Update GetProductCart.tsx

--- src/Components/GetProductCart.tsx
+++ src/Components/GetProductCart.tsx
@@ -1,9 +1,11 @@
+"use client";
 import React from "react";
 import { client } from "@/lib/sanityClient";
+import getStipePromise from "@/lib/stripe";
 
 const getProductById = async (id: any) => {
 
-    const res = await client.fetch(`
+  const res = await client.fetch(`
       *[_type == "product" && _id == "${id}"][0] {
         title,
         image,
@@ -14,21 +16,49 @@ const getProductById = async (id: any) => {
       }
     `);
 
-    return res;
-    
+  return res;
+
 };
 
 const GetProductCart = async ({ item }: any) => {
 
-    const product = await getProductById(item.product_id);
+  const product = await getProductById(item.product_id);
 
-    return (
-        <div>
-            <h1>
-                {product.title}
-            </h1>
-        </div>
-    )
+  const handleCheckOut = async () => {
+
+    const stripe = await getStipePromise();
+
+    const response = await fetch("/api/stripe/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      cache: "no-cache",
+      body: JSON.stringify(product)
+    });
+
+    const data = await response.json();
+
+    if (data.session) {
+      stripe?.redirectToCheckout({ sessionId: data.session.id });
+    }
+
+  };
+
+  return (
+    <div>
+      <h1>
+        {product.title}
+      </h1>
+
+      <button
+        className="mt-8 mb-5 py-3 px-6 sm:py-4 sm:px-8 rounded bg-black text-xl sm:text-2xl text-white font-bold"
+        onClick={handleCheckOut}
+      >
+        Check Out
+      </button>
+    </div>
+  )
 };
 
 export default GetProductCart;
