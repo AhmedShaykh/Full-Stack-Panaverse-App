1f66fe7e9929f5d0110ad66b4b610d917bd35f2f
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2023-07-15 21:58:05 +0500

    Final Commit

--- src/app/api/getcart/route.ts
+++ src/app/api/getcart/route.ts
@@ -1,20 +1,18 @@
 import { NextRequest, NextResponse } from "next/server";
-// import { cartTable, db } from "@/lib/drizzle";
-import { db } from '@vercel/postgres';
+import { cartTable, db } from "@/lib/drizzle";
 
 export const GET = async (request: NextRequest) => {
 
-    const client = await db.connect();
-
     try {
 
-        const res = await client.sql`SELECT * FROM Cart;`;
+        const res = await db.select().from(cartTable);
 
-        return NextResponse.json({ res: res.rows });
+        return NextResponse.json({ res });
 
     } catch (error) {
 
         return NextResponse.json({ message: "Something Went Wrong" });
 
     }
+
 };
