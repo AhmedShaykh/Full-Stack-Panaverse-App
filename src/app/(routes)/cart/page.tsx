9b3e2c2a89377b0453b2fbc9a5177e5afe25df4c
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2023-08-02 02:21:05 +0500

    Update page.tsx

--- src/app/(routes)/cart/page.tsx
+++ src/app/(routes)/cart/page.tsx
@@ -1,13 +1,44 @@
 import ProductCart from "@/Components/ProductCart";
+import { client } from "@/lib/sanityClient";
 
 const getCartData = async () => {
 
-    const res = await fetch(`http://localhost:3000//api/getcart`);
+    const res = await fetch(`http://localhost:3000/api/getcart`);
 
     const result = await res.json();
 
     return result;
 
+};
+
+const getProductByData = async (id: any) => {
+
+    const res = await client.fetch(`
+      *[_type == "product" && _id == '${id}'][0] {
+        title,
+        image,
+        price,
+        dresstype -> {
+          name
+        }
+      }
+    `);
+
+    return res;
+
+};
+
+const getData = async (data: any) => {
+
+    // const cartData: any = await getCartData();
+
+    console.log(data?.product_id);
+
+    return (
+        {/* <div className="wrapper">
+                <ProductCart item={data} />
+            </div> */}
+    )
 };
 
 const Cart = async () => {
@@ -26,9 +57,7 @@ const Cart = async () => {
 
     return (
         <>
-            <div className="wrapper">
-                <ProductCart item={data} />
-            </div>
+            {getData(data?.res[0])}
         </>
     )
 };
